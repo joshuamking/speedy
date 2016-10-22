126715b6fd670b45ad7f4c3ba1187412c4559a20
Joshua King <joshua@antechdevelopment.com>
2016-10-22 00:10:49 -0400

    Sorta finished step 2

--- app/src/main/java/com/speedy/ui/login/LoginActivity.java
+++ app/src/main/java/com/speedy/ui/login/LoginActivity.java
@@ -4,13 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import com.speedy.R;
-import com.speedy.api.FireBase;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
+import com.speedy.R;
+import com.speedy.api.FireBase;
 
 public class LoginActivity extends AppCompatActivity {
 
