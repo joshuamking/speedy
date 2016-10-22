030ff03bf2326090f63928f1787ae871df260577
Caleb Lewis <caleb_dre@yahoo.com>
2016-10-21 23:31:32 -0400

    Added butterknife

--- app/src/main/java/com/speedy/api/FireBase.java
+++ app/src/main/java/com/speedy/api/FireBase.java
@@ -1,4 +1,17 @@
 package com.speedy.api;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FireBase {
+
+    public FireBase() {
+    }
+
+    public void connect(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
 }
